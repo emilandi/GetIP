c115d623cc971836c8126c8e154fad06d746ba05
emilandi <emilandi2000@hotmail.com>
2020-03-04 09:53:15 -0300

    Update getIP.py

--- getIP.py
+++ getIP.py
@@ -1,5 +1,24 @@
+
+import socket
+from os import system
 from requests import get
 
-public = get('https://api.ipify.org').text
+system("cls")
 
+#consulta IP local y nombre host
+def getIP(): 
+    try: 
+        host = socket.gethostname() 
+        ip = socket.gethostbyname(host) 
+        print('')
+        print("Hostname :  ",host) 
+        print("Local IP : ",ip)
+        print('')
+    except: 
+        print("error no se puede resolver la consulta")   
+
+getIP()
+
+#IP Publica
+public = get('https://api.ipify.org').text
 print ('Public IP Address: ' +  public)
